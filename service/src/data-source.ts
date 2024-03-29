cf8a62cd8184d85e032db74231fcebac6def1b33
matsumo-and <yh134.toisanda@gmail.com>
2024-03-30 06:09:45 +0900

    [fix]db-env

--- service/src/data-source.ts
+++ service/src/data-source.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { User1711741565425 } from "./migration/1711741565425-User";
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
