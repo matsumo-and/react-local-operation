2a5ca13f0518e0e84e239e8754452b13d5a465ee
matsumo-and <yh134.toisanda@gmail.com>
2024-04-13 00:52:21 +0900

    [fix]es2017

--- service/src/data-source.ts
+++ service/src/data-source.ts
@@ -1,11 +1,10 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { User } from "./infrastructure/db/entity/User.js";
 import { User1711741565425 } from "./infrastructure/db/migration/1711741565425-User.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_ROOT_HOST,
@@ -14,8 +13,30 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   synchronize: true,
-  logging: false,
+  logging: true,
   entities: [User],
   migrations: [User1711741565425],
   subscribers: [],
 });
+
+/**
+ * typeORM Initialize
+ */
+await AppDataSource.initialize()
+  .then(async () => {
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.name = "test";
+    user.password = "test";
+    await AppDataSource.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await AppDataSource.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log(
+      "Here you can setup and run express / fastify / any other framework."
+    );
+  })
+  .catch((error) => console.log(error));
--- service/src/presentation/controller/api/v1/ros/CurrentPoseController.ts
+++ service/src/presentation/controller/api/v1/ros/CurrentPoseController.ts
@@ -2,6 +2,9 @@ import CurrentPoseUseCase from "../../../../../domain/usecase/ros/CurrentPoseUse
 import Coordinate from "../../../../../domain/model/Coordinate.js";
 import CurrentPoseResponse from "../../../../response/ros/CurrentPoseResponse.js";
 import express from "express";
+import { AppDataSource } from "../../../../../data-source.js";
+import { User } from "../../../../../infrastructure/db/entity/User.js";
+import dotenv from "dotenv";
 
 const currentPoseRouter = express.Router();
 
@@ -45,5 +48,19 @@ class CurrentPoseController {
     );
   }
 }
+
+/* currentpose router */
+currentPoseRouter.get(
+  "/test",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const users = await AppDataSource.manager.find(User);
+
+    res.json(users);
+  }
+);
 
 export default currentPoseRouter;
